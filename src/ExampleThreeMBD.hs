d890d67ef496a3783be53c06982e8e9be405d55b
Mario Román <mromang08@gmail.com>
2026-02-11 12:46:42 +0200

    example

--- src/ExampleThreeMBD.hs
+++ src/ExampleThreeMBD.hs
@@ -6,6 +6,9 @@ module ExampleThreeMBD where
 import Prelude hiding (id, (.), (>>=), (>>), return)
 import FinitaryMonad
 import MBD
+import Distribution (Distribution(..))
+import qualified Distribution as D
+import FrequentistTransformation
 
 data Health = Healthy | Ill deriving (Show, Eq)
 data TestResult = Positive | Negative deriving (Show, Eq)
@@ -21,10 +24,16 @@ test :: Health -> MBD TestResult
 test Healthy = distribution [(Positive, 5/100), (Negative, 95/100)]
 test Ill = distribution [(Positive, 90/100), (Negative, 10/100)]
 
+ep = do
+  p <- prior
+  r <- bag [Positive, Positive, Negative]
+  t <- test p
+  return (p,r,t)
+
 --experiment :: MBD Health
 -- close to what we want
 experiment = do
-  (r,p,t) <- do 
+  (r,p,t) <- do
     (r,p) <- do
         r <- bag [Positive, Positive, Negative]
         p <- prior
@@ -33,6 +42,19 @@ experiment = do
     return (r,p,t)
   observe (r == t)
   return (p, t, r)
+
+-- Jeffrey
+epm = do
+  (r,p,t) <- do
+    (r,p) <- do
+        r <- bag [Positive, Positive, Negative]
+        p <- prior
+        return (r,p)
+    t <- test p
+    return (r,p,t)
+  observe (r == t)
+  return p
+  
 
 experiment2 :: MBD Health
 experiment2 = do
@@ -45,3 +67,8 @@ experiment2 = do
         return (patient, result, seeing)
     observe (result == seeing)
     return patient
+
+
+collapsing :: (Eq a) => MBD a -> Maybe (Distribution a)
+collapsing (MBD Nothing) = Nothing
+collapsing (MBD (Just b)) = Just $ fJoin $ flrn b
